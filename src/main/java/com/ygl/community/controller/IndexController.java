70bb4be3579a8ca2dca32a9dbb133fb751616c4c
ygl <1539855225@qq.com>
2019-10-11 18:01:02 +0800

    add check cookies

--- src/main/java/com/ygl/community/controller/IndexController.java
+++ src/main/java/com/ygl/community/controller/IndexController.java
@@ -1,12 +1,37 @@
 package com.ygl.community.controller;
 
+import com.ygl.community.mapper.UserMapper;
+import com.ygl.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+
+    @Autowired
+    UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie:cookies){
+            if (cookie.getValue().equals("token")){
+                String token=cookie.getValue();
+                User  user=userMapper.findByToken(token);
+                if (user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
+
+
         return "index";
     }
 }
